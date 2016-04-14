8ac4c7216cc3a39655f5f1335bb2fa36b44f2ee1
Mikel Garcia <mikel_6@hotmail.com>
2016-04-14 17:49:09 +0200

    add Sinatra example

--- Sinatra example/server.rb
+++ Sinatra example/server.rb
@@ -1,18 +1,46 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/' do
-	@today = Date.today
-	erb(:tonto)
-end
+enable(:sessions)
 
-get '/hours/ago/:hours_ago' do
-	@hours = params[:hours_ago]
-	haml(:hours_ago)
+get '/' do
+	haml(:index)
 end
 
 get '/puertas/:name/:year' do
 	@name = params[:name]
 	@year = params[:year]
 	haml(:club_validator)
+end
+
+get '/puertas' do
+	@name = session[:name]
+	@year = session[:year]
+	haml(:club_validator)
+end
+
+get '/puertas/profile' do
+	haml(:club_profile)
+end
+
+get '/user/profile' do
+	@name = session[:name]
+	@year = session[:year]
+	haml(:profile)
+end
+
+post '/puertas/results' do
+	@name = params[:name]
+	@year = params[:year]
+	if params[:name].to_s.empty?
+		@error = "Error in name!"
+		haml(:club_profile)
+	elsif params[:year].to_s.empty?
+		@error = "Error in year!"
+		haml(:club_profile)
+	else
+		session[:name] = params[:name]
+		session[:year] = params[:year]
+		redirect to("/user/profile")
+	end
 end
